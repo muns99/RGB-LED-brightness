dfa1bfb5b385731330c918730b2e7f5c8dcc28bf
mohamed Mansour <mans75043@gmail.com>
2023-11-28 12:20:23 +0200

    Update PWM_program.c

--- HAL/pwm/PWM_program.c
+++ HAL/pwm/PWM_program.c
@@ -1,5 +1,5 @@
-#include "../../UTILITIES/STD_TYPES.h"
-#include "../../UTILITIES/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
 #include "../../MCAL/gpio/GPIO_interface.h"
 #include "../../MCAL/gpt/GPT_interface.h"
 #include "PWM_private.h"
@@ -16,10 +16,11 @@ enu_pwmError_t  PWM_init(void)
             && (str_g_pwmCFG.enu_pwmTimer <= PWM_TM5) && (str_g_pwmCFG.uint16_a_pwmFreq >= MIN_FREQ) 
             && (str_g_pwmCFG.uint16_a_pwmFreq <= MAX_FREQ) && (str_g_pwmCFG.uint8_a_pwmDC <= MAX_DC) )
             {
-                str_GPIOxPortInstance_t str_a_pwmPort = 
+                st_gpioPinConfig_t str_a_pwmPin = 
                 {
-                    .enu_PortBus = GPIO_PORT_BUS_APB,
-                    .enu_PortNumber = str_g_pwmCFG.enu_pwmPortNumber
+                    .port = str_g_pwmCFG.enu_pwmPortNumber,
+                    .pinNum = str_g_pwmCFG.enu_pwmPinNumber,
+                    .pinMode = DIGITAL_OUTPUT_2MA
                 };
                 str_timer_t str_a_pwmTimer = 
                 {
@@ -27,9 +28,7 @@ enu_pwmError_t  PWM_init(void)
                     .timerId = str_g_pwmCFG.enu_pwmTimer,
                     .timerMode = TIMER_PERIODIC
                 };
-                enu_funcRet |= MGPIO_init(&str_a_pwmPort);
-                enu_funcRet |= MGPIO_setPinType(&str_a_pwmPort, str_g_pwmCFG.enu_pwmPinNumber, GPIO_PINTYPE_DIGITAL_OUT);
-                enu_funcRet |= MGPIO_setPinValue(&str_a_pwmPort, str_g_pwmCFG.enu_pwmPinNumber, GPIO_PIN_LOW);
+                enu_funcRet |= GPIO_init(&str_a_pwmPin);
                 enu_funcRet |= GPT_initTimer(&str_a_pwmTimer);
                 enu_funcRet |= GPT_setTimerCallBack(str_a_pwmTimer.timerId, TIMER_CH_A, PWM_logic);
                 enu_funcRet |= GPT_enableTimerInterrupt(str_a_pwmTimer.timerId, GPT_TIMER_A_TIME_OUT);
@@ -71,21 +70,16 @@ enu_pwmError_t  PWM_turnOff()
 void PWM_logic(void)
 {
     static boolean periodFlag = FALSE;
-    str_GPIOxPortInstance_t str_a_pwmPort = 
-    {
-        .enu_PortBus = GPIO_PORT_BUS_APB,
-        .enu_PortNumber = str_g_pwmCFG.enu_pwmPortNumber
-    };
     if (periodFlag == TRUE)
     {
         GPT_preloadTimer(str_g_pwmCFG.enu_pwmTimer, TIMER_CH_A, TIMER_MICROSECONDS, str_g_pwmCFG.uint16_a_Onticks);
-        MGPIO_TogglePin(&str_a_pwmPort, str_g_pwmCFG.enu_pwmPinNumber);
+        GPIO_togglePin(str_g_pwmCFG.enu_pwmPortNumber,str_g_pwmCFG.enu_pwmPinNumber);
         periodFlag = FALSE;
     }
     else if(periodFlag == FALSE)
     {
         GPT_preloadTimer(str_g_pwmCFG.enu_pwmTimer, TIMER_CH_A, TIMER_MICROSECONDS, ( (TICK_TIME_FACTOR / str_g_pwmCFG.uint16_a_pwmFreq) - str_g_pwmCFG.uint16_a_Onticks)  );
-        MGPIO_TogglePin(&str_a_pwmPort, str_g_pwmCFG.enu_pwmPinNumber);
+        GPIO_togglePin(str_g_pwmCFG.enu_pwmPortNumber,str_g_pwmCFG.enu_pwmPinNumber);
         periodFlag = TRUE;
     }
 }
