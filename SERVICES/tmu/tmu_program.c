f2e93f269a4213ffa5e0345b3580943c32a2483c
mohamed Mansour <mans75043@gmail.com>
2023-12-14 12:16:22 +0200

    add MISRA changes | tmu

--- SERVICES/tmu/tmu_program.c
+++ SERVICES/tmu/tmu_program.c
@@ -17,19 +17,35 @@ st_tmuManage_t st_gl_tmuManage[TMU_INSTANCES];
 enu_tmuErrorState_t TMU_createTask(st_tmuTask_t *st_a_taskInstance)
 {
     enu_tmuErrorState_t enu_a_functionRet   = TMU_SUCCESS;
+    sint8_t uint8_a_firstFreeTmuInstance;
+    uint64_t uint64_a_taskInterval;
     if (uint8_gl_manageCenterCounter < TMU_INSTANCES)
     {
         if (st_a_taskInstance != NULL)
         {
             if (st_a_taskInstance->uint8_a_taskId < TMU_INSTANCES)
             {
-                sint8_t uint8_a_firstFreeTmuInstance;
                 for ( uint8_a_firstFreeTmuInstance = 0; uint8_a_firstFreeTmuInstance < TMU_INSTANCES; uint8_a_firstFreeTmuInstance++)
                 {
                     if (st_gl_tmuInstances[uint8_a_firstFreeTmuInstance].state == TMU_INSTANCE_NOT_INITIALIZED)
                     {
-                        uint64_t uint64_a_taskInterval;
-                        uint64_a_taskInterval = st_a_taskInstance->enu_a_scale == MICRO?(st_a_taskInstance->taskInterval / 1000):(st_a_taskInstance->enu_a_scale == MILLI?st_a_taskInstance->taskInterval:st_a_taskInstance->taskInterval * 1000); 
+                        
+                        //uint64_a_taskInterval = st_a_taskInstance->enu_a_scale == MICRO?(st_a_taskInstance->taskInterval / 1000):(st_a_taskInstance->enu_a_scale == MILLI?st_a_taskInstance->taskInterval:st_a_taskInstance->taskInterval * 1000); 
+
+                        if (st_a_taskInstance->enu_a_scale == MICRO) 
+                        {
+                            uint64_a_taskInterval = st_a_taskInstance->taskInterval * 1000;
+                        } 
+                        else if (st_a_taskInstance->enu_a_scale == MILLI) 
+                        {
+                            uint64_a_taskInterval = st_a_taskInstance->taskInterval;
+                        } 
+                        else 
+                        {
+                            uint64_a_taskInterval = st_a_taskInstance->taskInterval / 1000;
+                        }
+                        
+                        
                         if (uint64_a_taskInterval <= st_gl_tmuInstances[uint8_a_firstFreeTmuInstance].maxTime)
                         {
                             break;
@@ -90,25 +106,29 @@ enu_tmuErrorState_t TMU_createTask(st_tmuTask_t *st_a_taskInstance)
 enu_tmuErrorState_t TMU_startTask(uint8_t *uint8_a_taskId,uint64_t *uint64_a_interval)
 {
     enu_tmuErrorState_t enu_a_functionRet = TMU_SUCCESS;
+    uint8_t uint8_a_taskIndex ;
+    st_tmuTask_t *st_a_tempTaskInstance;
+    st_tmu_t *st_a_tmuTempInstance;
+    str_timer_t *st_a_tempTimerInstance;
     if (uint8_a_taskId != NULL)
     {
         if (*uint8_a_taskId < TMU_INSTANCES)
         {
             if (uint64_a_interval != NULL)
             {
-                uint8_t uint8_a_taskIndex ;
+                
                 for (uint8_a_taskIndex = 0; uint8_a_taskIndex <= uint8_gl_manageCenterCounter; uint8_a_taskIndex++)
                 {
-                    st_tmuTask_t *st_a_tempTaskInstance = st_gl_tmuManage[uint8_a_taskIndex].tmutask;
+                    st_a_tempTaskInstance = st_gl_tmuManage[uint8_a_taskIndex].tmutask;
                     if (st_a_tempTaskInstance->uint8_a_taskId == *uint8_a_taskId)
                     {
-                        st_tmu_t *st_a_tmuTempInstance = st_gl_tmuManage[uint8_a_taskIndex].tmuInstance;
+                        st_a_tmuTempInstance = st_gl_tmuManage[uint8_a_taskIndex].tmuInstance;
 
                         if (st_a_tmuTempInstance->state == TMU_INSTANCE_INITIALIZED || st_a_tmuTempInstance->state == TMU_INSTANCE_RUNNING)
                         {
                             if(st_a_tmuTempInstance->maxTime >= *uint64_a_interval)
                             {
-                                str_timer_t *st_a_tempTimerInstance = &st_a_tmuTempInstance->timer;
+                                st_a_tempTimerInstance = &st_a_tmuTempInstance->timer;
                                 enu_a_functionRet = GPT_preloadTimer(st_a_tempTimerInstance->timerId,st_a_tempTimerInstance->timerChannel,st_a_tempTaskInstance->enu_a_scale,*uint64_a_interval) == GPT_OK?enu_a_functionRet : TMU_NOT_SUCCESS;
                                 enu_a_functionRet = GPT_enableTimerInterrupt(st_a_tempTimerInstance->timerId,st_a_tmuTempInstance->timerInterrupt);
                                 enu_a_functionRet = GPT_startTimer(st_a_tempTimerInstance->timerId,st_a_tempTimerInstance->timerChannel);
@@ -158,6 +178,9 @@ enu_tmuErrorState_t TMU_startTask(uint8_t *uint8_a_taskId,uint64_t *uint64_a_int
 enu_tmuErrorState_t TMU_stopTask(uint8_t *uint8_a_taskId)
 {
     enu_tmuErrorState_t enu_a_functionRet = TMU_SUCCESS;
+    st_tmuTask_t *st_a_tempTaskInstance;
+    st_tmu_t *st_a_tmuTempInstance;
+    str_timer_t *st_a_tempTimerInstance;
     if (uint8_a_taskId != NULL)
     {
         if (uint8_a_taskId < TMU_INSTANCES)
@@ -165,14 +188,14 @@ enu_tmuErrorState_t TMU_stopTask(uint8_t *uint8_a_taskId)
             uint8_t uint8_a_taskIndex ;
             for (uint8_a_taskIndex = 0; uint8_a_taskIndex <= uint8_gl_manageCenterCounter; uint8_a_taskIndex++)
             {
-                st_tmuTask_t *st_a_tempTaskInstance = st_gl_tmuManage[uint8_a_taskIndex].tmutask;
+                st_a_tempTaskInstance = st_gl_tmuManage[uint8_a_taskIndex].tmutask;
                 if (st_a_tempTaskInstance->uint8_a_taskId == uint8_a_taskId)
                 {
-                    st_tmu_t *st_a_tmuTempInstance = st_gl_tmuManage[uint8_a_taskIndex].tmuInstance;
+                    st_a_tmuTempInstance = st_gl_tmuManage[uint8_a_taskIndex].tmuInstance;
 
                     if (st_a_tmuTempInstance->state == TMU_INSTANCE_RUNNING)
                     {
-                        str_timer_t *st_a_tempTimerInstance = &st_a_tmuTempInstance->timer;
+                        st_a_tempTimerInstance = &st_a_tmuTempInstance->timer;
                          enu_a_functionRet = GPT_startTimer(st_a_tempTimerInstance->timerId,st_a_tempTimerInstance->timerChannel);
                         enu_a_functionRet = GPT_disableTimerInterrupt(st_a_tempTimerInstance->timerId,st_a_tmuTempInstance->timerInterrupt);
                         st_a_tmuTempInstance->state = enu_a_functionRet == TMU_SUCCESS?TMU_INSTANCE_RUNNING : st_a_tmuTempInstance->state;
